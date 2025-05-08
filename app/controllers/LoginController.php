1885050764b6cad5b0286beac6086c8d253c3d30
igorxoi <igorx92@gmail.com>
2025-05-08 19:53:41 -0300

    refactor: corrigindo indentação

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -4,35 +4,30 @@ session_start();
 require_once __DIR__ . '/../../core/Controller.php';
 
 class LoginController extends Controller {
-    public function index() {
-        require_once __DIR__ . '/../views/login.php';
-    }
+	public function index() {
+		require_once __DIR__ . '/../views/login.php';
+	}
 
-    public function authenticate() {
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+	public function authenticate() {
+		$email = $_POST['email'];
+		$senha = $_POST['senha'];
 
-        $userModel = $this->model('User');
-        $user = $userModel->checkCredentials($email, $senha);
-        
-        if ($user) {
-            $permissions = $userModel->getPermissions($user['id']);
+		$userModel = $this->model('User');
+		$user = $userModel->checkCredentials($email, $senha);
+		
+		if ($user) {
+			$permissions = $userModel->getPermissions($user['id']);
 
-            $_SESSION['user'] = [
-                'id' => $user['id'],
-                'nome' => $user['nome'],
-                'permissoes' => $permissions
-            ];
+			$_SESSION['user'] = [
+				'id' => $user['id'],
+				'nome' => $user['nome'],
+				'permissoes' => $permissions
+			];
 
-            // print_r($_SESSION['user']['id']);
-            // print_r($_SESSION['user']['nome']);
-
-            // print_r($_SESSION['user']['permissoes'][0]);
-
-            header("Location: index.php?url=estacionamento/gerenciar");
-            exit;
-        } else {
-            echo "Credenciais inválidas.";
-        }
-    }
+			header("Location: index.php?url=estacionamento/gerenciar");
+			exit;
+		} else {
+			echo "Credenciais inválidas.";
+		}
+	}
 }
