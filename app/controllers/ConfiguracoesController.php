779239fc3126247e8432c675b64f2313a0e3f983
igorxoi <igorx92@gmail.com>
2025-05-16 12:33:53 -0300

    feat: adicionando adição de tarifa

--- app/controllers/ConfiguracoesController.php
+++ app/controllers/ConfiguracoesController.php
@@ -2,12 +2,29 @@
 
 require_once __DIR__ . '/../../core/Controller.php';
 
-class ConfiguracoesController extends Controller {
-	public function index() {
+class ConfiguracoesController extends Controller
+{
+	public function index()
+	{
 		require_once __DIR__ . '/../views/configuracoes.php';
 	}
 
-	public function gerenciarVagas() {
-		print_r($_POST);
+	public function gerenciarVagas()
+	{
+		$dados = [
+			'tipo_vaga_id' => $_POST['tipo'] ?? '',
+			'valor_primeira_hora' => $_POST['valor_primeira_hora'] ?? '',
+			'valor_demais_horas' => $_POST['valor_demais_horas'] ?? '',
+		];
+
+		$configuracoesModel = $this->model('Configuracoes');
+		$tarifa_id = $configuracoesModel->adicionarTarifa($dados);
+
+		if ($tarifa_id) {
+			header("Location: index.php?url=configuracoes/index");
+			exit;
+		} else {
+			echo "Credenciais inválidas.";
+		}
 	}
 }
